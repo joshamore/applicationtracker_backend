ab66bd5bc35957066237476dd72bc4906a34f4db
Josh Amore <joshamore31@gmail.com>
2020-08-04 09:01:41 +1000

    Added comments

--- helpers/verifyToken.js
+++ helpers/verifyToken.js
@@ -1,6 +1,9 @@
 const jwt = require("jsonwebtoken");
 const CONSTANTS = require(".././CONSTANTS");
 
+/**
+ * Verifies a JWT token
+ */
 module.exports = function (req, res, next) {
 	// Getting token from req
 	const token = req.header("auth-token");
@@ -11,10 +14,13 @@ module.exports = function (req, res, next) {
 	}
 
 	try {
+		// If verification possible, adding to req and calling next middleware
 		const verified = jwt.verify(token, CONSTANTS.auth.JWT_SECRET);
 		req.user = verified;
+
 		next();
 	} catch (err) {
+		// If token can't be verified, responding with error
 		res.status(400).json({ error: `Auth error: ${err}` });
 	}
 };
